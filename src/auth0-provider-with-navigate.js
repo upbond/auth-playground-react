1d475c97e5b3fd43a65040634a6e9885c65b848e
Yuri Ardila <ardiyu07@gmail.com>
2024-03-29 10:47:58 +0700

    Changed port

--- src/auth0-provider-with-navigate.js
+++ src/auth0-provider-with-navigate.js
@@ -2,12 +2,12 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-export const Auth0ProviderWithNavigate = ({ children }) => {
+export const AuthProviderWithNavigate = ({ children }) => {
   const navigate = useNavigate();
 
-  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-  const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
+  const domain = process.env.REACT_APP_LOGIN30_DOMAIN;
+  const clientId = process.env.REACT_APP_LOGIN30_CLIENT_ID;
+  const redirectUri = process.env.REACT_APP_LOGIN30_CALLBACK_URL;
 
   const onRedirectCallback = (appState) => {
     navigate(appState?.returnTo || window.location.pathname);
@@ -23,6 +23,7 @@ export const Auth0ProviderWithNavigate = ({ children }) => {
       clientId={clientId}
       authorizationParams={{
         redirect_uri: redirectUri,
+        scope: ''
       }}
       onRedirectCallback={onRedirectCallback}
     >
