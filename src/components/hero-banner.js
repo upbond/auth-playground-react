8f14b8907d032340aa4317a1d63b9ee4e61ccc44
Rahmatullah Arrizal <rahmatullaharrizal@gmail.com>
2024-07-09 09:43:10 +0700

    fix class name

--- src/components/hero-banner.js
+++ src/components/hero-banner.js
@@ -6,10 +6,10 @@ export const HeroBanner = () => {
   return (
     <div className="hero-banner hero-banner--pink-yellow">
       <div className="hero-banner__logo">
-      <img class="mb-3 app-logo" src="https://da7udebijaype.cloudfront.net/uploads/startups/logos/c915d3a2-faa5-47c1-952c-45432b0ef32f.png?=1661886060" alt="upbondlogo" width="120" />
+      <img className="mb-3 app-logo" src="https://da7udebijaype.cloudfront.net/uploads/startups/logos/c915d3a2-faa5-47c1-952c-45432b0ef32f.png?=1661886060" alt="upbondlogo" width="120" />
       </div>
       <h1 className="hero-banner__headline">UPBOND LOGIN3.0</h1>
-      <p class="hero-banner__description">
+      <p className="hero-banner__description">
       UpbondのLogin 3.0 SDKを使用した認証フローを示すサンプルアプリケーションです。
     </p>
       <a
